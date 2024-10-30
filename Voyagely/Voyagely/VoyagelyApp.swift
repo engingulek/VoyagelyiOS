a0f536e79369b8bc73b1091d2aafc04084e3295f
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2024-10-30 17:36:14 +0300

    created init project

---
+++ Voyagely/Voyagely/VoyagelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyagelyApp.swift
+//  Voyagely
+//
+//  Created by Engin Gülek on 30.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct VoyagelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
